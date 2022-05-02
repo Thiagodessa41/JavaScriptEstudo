a7eff04baa95eb84949c1499f4e9beef7b4f640f
Thiago <thiagodessa41@gmail.com>
2022-05-02 20:18:02 -0300

    condicao

--- Aula10/aula 11/ex008.js
+++ Aula10/aula 11/ex008.js
@@ -5,3 +5,12 @@ if (vel>60) {
 console.log('Voce ultrapassou a velocidade de 60km/h MULTADO!')
 }
 console.log('Dirija com cuidado use o cinto de segurança!')
+
+
+var pais = 'França'
+console.log(`Vivendo em ${pais} `)
+if (pais != Brasil){
+    console.log('Voce é estrangeiro')
+}else{
+    console.log('Voce é brasileiro')
+}
